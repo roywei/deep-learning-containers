dfadc30b64fc965aa66290d549158daef805af48
Lai Wei <royweilai@gmail.com>
2022-02-24 15:51:08 -0800

    disable single node test

--- test/sagemaker_tests/pytorch/training/resources/mnist/smdataparallel_smmodelparallel_mnist_script_mode.sh
+++ test/sagemaker_tests/pytorch/training/resources/mnist/smdataparallel_smmodelparallel_mnist_script_mode.sh
@@ -3,7 +3,8 @@
 # SPDX-License-Identifier: MIT-0
 
 set -ex
-
-smddprun python smdataparallel_mnist.py
+# SMDDPv1.4.0 no longer supports single node mode, it's for internal debug only
+# no user logic will use this function
+#smddpsinglenode python smdataparallel_mnist.py
 
 bash smmodelparallel_mnist_script_mode.sh
